3e43360dae5635ab58a02672dec13f1b6411800d
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-02-07 17:22:56 +0530

    Web pack

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const currLocation = document.querySelector("button");
+
+currLocation.addEventListener("click", cuLocation);
+
+function cuLocation(){
+    console.log("current location");
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+let longitude = 0;
+let latitude = 0;
+let apiKey = '02cd7df6f57621292521348f6a56ec7c'
+function showPosition(position){
+     longitude = position.coords.longitude;
+     latitude = position.coords.latitude;
+
+    console.log(longitude, latitude);
+    fetchData();
+}
+
+
+
+  function fetchData(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`, {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response);
+    });
+  }
